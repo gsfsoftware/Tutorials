014457567b1bb46ccebc955a7531b1e496e6a7f8
RemoteGraham <graham.mcphee@gsfsoftware.co.uk>
2021-07-31 12:39:11 +0100

    YT ThirdParty_SQL_MLG_Part3

---
+++ ThirdParty_SQL_MLG_Part3/Code/SQL_scripts/UserFile.sql
@@ -0,0 +1,209 @@
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Samuel','Hogan','1955-10-07','Senior manager','IT','211 Any Road','Any Town','AN10 7PE','0438 0473 0285','Samuel.Hogan@gmail.com',65)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Dale','Blanchard','1983-08-01','Team member','Payroll','973 Main Street','Green Acres','GR3 9IP','0382 0103 0276','Dale.Blanchard@gmail.com',37)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Christina','Kline','1934-11-15','Manager','IT','456 Any Crescent','Medium City','ME7 8OQ','0298 0487 0883','Christina.Kline@gmail.com',86)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Laura','Winters','1983-03-25','Manager','HR','300 Long Road','New Town','NE37 8JY','0489 0684 0598','Laura.Winters@gmail.com',38)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Danielle','Holden','1946-02-22','Junior member','Payroll','305 Orange Avenue','Big City','BI47 6JV','0501 0213 0148','Danielle.Holden@gmail.com',75)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Sean','Oconnor','1983-12-09','Team member','HR','794 Orange Avenue','Green Acres','GR4 4LX','0473 0410 0658','Sean.Oconnor@gmail.com',37)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Stuart','Saunders','1953-05-19','Team leader','IT','246 Any Avenue','Big City','BI35 2FU','0362 0204 0873','Stuart.Saunders@gmail.com',68)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Robyn','Huber','1950-07-07','Junior member','IT','382 Short Road','Green Acres','GR6 5UZ','0238 0573 0774','Robyn.Huber@gmail.com',71)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Louise','Gutierrez','1984-12-21','Senior manager','Facilities','743 Any Square','New Town','NE47 3AK','0227 0394 0106','Louise.Gutierrez@gmail.com',36)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Neil','Warner','1940-12-16','Team leader','Facilities','563 Short Road','Jonesville','JO17 6BJ','0477 0541 0214','Neil.Warner@gmail.com',80)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Scott','Travis','1962-01-26','Team member','IT','681 Any Square','Joesville','JO9 4KA','0561 0294 0516','Scott.Travis@gmail.com',59)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Jason','Park','1995-04-13','Manager','Server Support','177 Any Avenue','Midtown','MI32 6KS','0536 0249 0423','Jason.Park@gmail.com',26)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Angela','Bolton','1992-02-13','Junior member','Facilities','97 Any Road','Big City','BI41 1ON','0494 0370 0108','Angela.Bolton@gmail.com',29)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Lee','Ashley','1999-01-03','Team leader','IT','813 Green Avenve','HopeTown','HO48 1RZ','0211 0295 0434','Lee.Ashley@gmail.com',22)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Elizabeth','Oneill','1998-06-23','Junior member','IT','125 Short Road','New Town','NE22 4RC','0399 0466 0891','Elizabeth.Oneill@gmail.com',23)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Kyle','Davidson','1989-04-21','Team member','HR','472 Tower Hamlet','Jonesville','JO33 1CK','0346 0815 0553','Kyle.Davidson@gmail.com',32)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Matthew','Frost','1943-05-15','Junior member','Server Support','855 Short Road','Meaddowsville','ME47 1QQ','0357 0382 0802','Matthew.Frost@gmail.com',78)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Anthony','Mora','1937-07-11','Senior manager','Payroll','308 Argyll Gardens','HopeTown','HO46 2KL','0285 0861 0050','Anthony.Mora@gmail.com',84)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'William','Ortiz','1941-04-18','Senior manager','Facilities','921 Long Road','Any Town','AN35 5JR','0373 0181 0018','William.Ortiz@gmail.com',80)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Maisie','Carney','1967-09-08','Team leader','IT','762 Any Crescent','Midtown','MI15 3UG','0499 0563 0900','Maisie.Carney@gmail.com',53)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Clare','Hawkins','1975-02-04','Team member','Payroll','903 Medium Road','Midtown','MI31 2VD','0252 0645 0633','Clare.Hawkins@gmail.com',46)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Derek','Hart','1986-07-08','Team member','Payroll','254 Main Street','Meaddowsville','ME47 1JG','0598 0751 0697','Derek.Hart@gmail.com',35)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Charles','Galloway','1970-10-10','Manager','Facilities','446 Main Street','Any Town','AN2 1BN','0556 0337 0777','Charles.Galloway@gmail.com',50)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Gavin','Russell','1988-06-27','Team leader','Payroll','323 Freen Road','Midtown','MI7 6OR','0247 0108 0655','Gavin.Russell@gmail.com',33)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Siobhan','Ellis','1969-05-17','Senior manager','Facilities','932 Medium Road','Medium City','ME32 4UI','0329 0724 0394','Siobhan.Ellis@gmail.com',52)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Elizabeth','Buck','1993-04-02','Junior member','Facilities','290 Blue Street','Midtown','MI13 3IT','0285 0184 0332','Elizabeth.Buck@gmail.com',28)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Kenneth','Herring','1969-10-03','Team leader','Server Support','508 Any Avenue','Meaddowsville','ME17 3AS','0246 0561 0729','Kenneth.Herring@gmail.com',51)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Louise','Acevedo','1988-10-11','Team member','Facilities','620 Medium Road','New Town','NE30 6DH','0334 0432 0285','Louise.Acevedo@gmail.com',32)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Heather','Wong','1964-05-09','Team leader','IT','928 Main Street','HopeTown','HO6 5WA','0284 0336 0562','Heather.Wong@gmail.com',57)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Louise','Christensen','1951-06-06','Senior manager','IT','1030 Tower Hamlet','Joesville','JO30 9AJ','0409 0790 0875','Louise.Christensen@gmail.com',70)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Fraser','Griffin','1987-04-18','Team member','IT','1150 Green Avenue','HopeTown','HO30 3KH','0231 0805 0049','Fraser.Griffin@gmail.com',34)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'William','Clay','1972-06-06','Team member','Payroll','911 Purple Square','New Town','NE28 4KZ','0346 0440 0508','William.Clay@gmail.com',49)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Jonathan','Allen','1973-02-08','Senior manager','Payroll','405 Medium Road','New Town','NE9 2NN','0406 0675 0854','Jonathan.Allen@gmail.com',48)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Alfie','Kelly','1995-06-11','Junior member','Facilities','509 Any Road','New City','NE1 4NQ','0271 0315 0489','Alfie.Kelly@gmail.com',26)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Esme','Wong','1967-11-19','Manager','Payroll','247 Medium Road','Jonesville','JO36 1JM','0331 0409 0548','Esme.Wong@gmail.com',53)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Layla','Whitaker','1979-06-28','Team leader','Payroll','149 Purple Square','New Town','NE27 7HK','0574 0586 0608','Layla.Whitaker@gmail.com',42)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Stuart','Jenkins','1952-04-14','Manager','Payroll','296 Purple Square','Meaddowsville','ME7 3AR','0411 0112 0425','Stuart.Jenkins@gmail.com',69)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Duncan','French','1963-11-10','Manager','Server Support','907 Main Street','Joesville','JO11 5LX','0286 0261 0873','Duncan.French@gmail.com',57)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Oscar','Paul','1955-04-28','Team member','HR','1194 Freen Road','Medium City','ME44 7TH','0274 0875 0676','Oscar.Paul@gmail.com',66)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Christina','Valdez','1959-02-01','Team member','Server Support','1084 Tower Hamlet','Any Town','AN1 7HU','0504 0414 0719','Christina.Valdez@gmail.com',62)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Steven','Gardner','1932-12-03','Junior member','Server Support','593 Freen Road','Any Town','AN4 2YV','0329 0412 0109','Steven.Gardner@gmail.com',88)
+GO
+
+Insert into dbo.tbl_UserList
+(Active,FirstName,Surname,DOB,JobTitle,Department,street,city,postcode,Telephone,Email,Age)
+Values(1,'Grace','Allen','1949-11-27','Junior member','IT','605 Any Road','Joesville','JO28 6MW','0521 0468 0592','Grace.Allen@gmail.com',71)
+GO
